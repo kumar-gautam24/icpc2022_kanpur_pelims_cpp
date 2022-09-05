cbe2af66cacf74c11a1fd56bd3c17fcdf7f56a28
Gautam Kumar <gk39951@gmail.com>
2022-09-05 11:59:47 +0530

    Update icpc_operation_onstring.cpp

--- icpc_operation_onstring.cpp
+++ icpc_operation_onstring.cpp
@@ -19,7 +19,7 @@ int32_t main()
         string s;
         cin >> s;
         int ans = 0;
-        vector<int> block;
+        vector<int> block; // stores all the block of contionouus '0'
         for (int i = 0; i < n; i++)
         {
             if (s[i] == '1')
@@ -34,20 +34,20 @@ int32_t main()
                     count++;
                     i++;
                 }
-                block.push_back(count);
+                block.push_back(count); // push the length of block 
                 count = 0;
                 i--;
             }
         }
-        sort(block.begin(), block.end(), greater<int>());
+        sort(block.begin(), block.end(), greater<int>()); // sort the block in deseceding order
         for (auto it : block)
         {
-            if (x > 2 * it)
+            if (x > 2 * it) // if entire block can be flipped 
             {
                 ans += it;
                 x = x - (2 * it + 1);
             }
-            else
+            else  // if  we have less coin then only flip how much coin we have
             {
                 ans += (x - 1) / 2;
                 break;
